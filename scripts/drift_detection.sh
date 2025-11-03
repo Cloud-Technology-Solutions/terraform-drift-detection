11c2109f70f1ea3145dcf9ea1c334445b3d72ac9
Hazi Florin - Marian <florin.hazi@appsbroker.com>
2025-11-03 11:35:13 +0200

    Updated cloud build

--- scripts/drift_detection.sh
+++ scripts/drift_detection.sh
@@ -83,7 +83,7 @@ check_terragrunt_drift() {
     return 1
   fi
 
-  if terragrunt plan -detailed-exitcode -no-color -lock=false --non-interactive > /dev/null 2>&1; then
+  if terragrunt plan -detailed-exitcode -no-color -lock=false --non-interactive then
     return 0
   else
     local exit_code=$?
